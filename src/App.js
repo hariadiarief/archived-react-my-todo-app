d73dff30c9689e76137d268843f3ccbce79d156a
masagus hariadi arief <adi.arief77@gmail.com>
2019-03-01 15:14:18 +0700

    make form and submit button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <h1>Todo React</h1>
+          <form action="">
+            <input type="text" placeholder="what will you do ?" />
+            <input type="submit" value="Add Todo" />
+          </form>
+        </div>
       </div>
     );
   }
