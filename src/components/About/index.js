2e1e99c1e130af9d0bb2958e8fb12aa707887711
masagus hariadi arief <adi.arief77@gmail.com>
2019-03-13 10:51:06 +0700

    change content of about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -20,7 +20,75 @@ class About extends React.Component {
     return (
       <Page>
         <Segment className="gradientBackground">
+          {/*  */}
           <Particles
+            className="partikel"
+            params={{
+              fps_limit: 28,
+              particles: {
+                number: {
+                  value: 200,
+                  density: {
+                    enable: false
+                  }
+                },
+                line_linked: {
+                  enable: true,
+                  distance: 30,
+                  opacity: 0.4
+                },
+                move: {
+                  speed: 1
+                },
+                opacity: {
+                  anim: {
+                    enable: true,
+                    opacity_min: 0.05,
+                    speed: 2,
+                    sync: false
+                  },
+                  value: 0.4
+                }
+              },
+              polygon: {
+                enable: true,
+                scale: 0.5,
+                type: "inline",
+                move: {
+                  radius: 10
+                },
+                // url: "/assets/image/3176784.svg",
+                url: "/assets/image/small-deer.2a0425af.svg",
+                inline: {
+                  arrangement: "equidistant"
+                },
+                draw: {
+                  enable: true,
+                  stroke: {
+                    color: "rgba(255, 255, 255, .2)"
+                  }
+                }
+              },
+              retina_detect: false,
+              interactivity: {
+                events: {
+                  onhover: {
+                    enable: true,
+                    mode: "bubble"
+                  }
+                },
+                modes: {
+                  bubble: {
+                    size: 6,
+                    distance: 40
+                  }
+                }
+              }
+            }}
+          />
+          {/*  */}
+
+          {/* <Particles
             className="partikel"
             params={{
               particles: {
@@ -33,7 +101,7 @@ class About extends React.Component {
                 }
               }
             }}
-          />
+          /> */}
           <div className="containerAbout">
             <div className="contentAbout">
               <Typed
